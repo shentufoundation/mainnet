f2a83a0f250c16b77bc540c60d95c86b57d121e5
zheng-bin <wenwubinzheng@gmail.com>
2024-03-22 16:46:58 +0800

    Update state_sync.sh

--- shentu-2.2/statesync/state_sync.sh
+++ shentu-2.2/statesync/state_sync.sh
@@ -4,7 +4,6 @@ set -ux
 set -e
 set -v
 
-# Please, update to RPC_ADDR to 3.236.161.42:26657 if you like to use Certik official RPC Node
 # Given address is DSRV RPC Node Address.
 RPC_ADDR="165.232.72.33:26657"
 INTERVAL=1500
